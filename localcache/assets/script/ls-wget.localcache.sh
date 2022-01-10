fac9fe1db2fe0c4f4c7c2e65b1c83d1e697fbf96
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2022-01-10 10:44:05 +0800

    Update ls-wget.localcache.sh

--- localcache/assets/script/ls-wget.localcache.sh
+++ localcache/assets/script/ls-wget.localcache.sh
@@ -1,2 +1,2 @@
 nginxroot=/c/cache/software-for-development
-find $pwd -type f | xargs readlink -f | sed "s/.\{${#nginxroot}\}\(.\{1,\}\)/wget http:\/\/localcache\1/"
+find $pwd -type f | xargs readlink -f | sed "s/.\{${#nginxroot}\}\(.\{1,\}\)/wget http:\/\/localcache\1/"  > localcache.info
